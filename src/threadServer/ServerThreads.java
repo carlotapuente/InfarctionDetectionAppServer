355a3d711f7c40cbe7c8ff4a312b1a025fa7509e
carlo <carlo@10.60.84.224>
2022-11-28 19:53:30 +0100

    thread to close the server

--- src/threadServer/ServerThreads.java
+++ src/threadServer/ServerThreads.java
@@ -9,45 +9,113 @@ package threadServer;
  *
  * @author carlo
  */
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import ui.InputOutput;
 
 public class ServerThreads {
 
+    private static List<Thread> threadList = new ArrayList();
+    private static ServerSocket serverSocket;
+    private static Socket socket;
+
     public static void main(String args[]) throws IOException {
         //Create a service that is waiting in port 9000
-        ServerSocket serverSocket = new ServerSocket(9000);
-        //Socket socket1 = serverSocket.accept();
-        //new Thread(new ThreadToReadFromKeyboard(socket1).start());      
 
-            while (true) {
-                    //Thie executes when we have a client
-                    Socket socket = serverSocket.accept();
-                    new Thread(new ServerThreadsClient(socket)).start();
-
-            }
-
+        serverSocket = new ServerSocket(9000);
+        Socket socket1 = serverSocket.accept();
+        new Thread(new ThreadToReadFromKeyboard(socket1)).start();  
         
+        String option;
+
+        while (true) {
+            //Thie executes when we have a client
+            Socket socket = serverSocket.accept();
+            Thread t = new Thread(new ServerThreadsClient(socket));
+            t.start();
+            threadList.add(t);
+        
+
+            System.out.println("1. Close the server.");
+            option = InputOutput.get_String();
+
+            /*if (option.equals("1")) {
+                System.out.println("Admin password: \n");
+                String password = InputOutput.get_String();
+                if (password.equals("12345")) {
+                    if (sendClientsAlive() == 0) {
+                        System.out.println("There are no clients connected.\n The server has been closed successfully");
+                        releaseResourcesServer(getServerSocket());
+                        //System.exit(0);
+                    } else {
+                        System.out.println("There are " + sendClientsAlive() + " clients still connected. \n Are you sure you want to close the server? Y/N");
+                        option = InputOutput.get_String();
+                        if (option.equals("Y")) {
+                            System.out.println("The server has been closed successfully");
+                            releaseResourcesServer(getServerSocket(), socket);
+
+                            //System.exit(0);
+                        }
+                    }
+                }
+            }*/
+        }
+            
+        
+
         /*finally {
             releaseResourcesServer(serverSocket);
         }*/
     }
 
     
+    public static ServerSocket getServerSocket() {
+        return serverSocket;
+    }
+    
+    public static Socket getSocket() {
+        return socket;
+    }
 
-    private static void releaseResourcesServer(ServerSocket serverSocket) {
+    public static int sendClientsAlive() {
+        int contador = 0;
+
+        for (int i = 0; i < threadList.size(); i++) {
+            if (threadList.get(i).isAlive()) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public static void releaseResourcesServer(ServerSocket serverSocket, Socket socket) {
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerThreads.class.getName()).log(Level.SEVERE, null, ex);
+        }
         try {
             serverSocket.close();
         } catch (IOException ex) {
             Logger.getLogger(ServerThreads.class.getName()).log(Level.SEVERE, null, ex);
         }
+
     }
+    
+        public static void releaseResourcesServer(ServerSocket serverSocket) {
+        try {
+            serverSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerThreads.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
+}
--- src/threadServer/ThreadToReadFromKeyboard.java
+++ src/threadServer/ThreadToReadFromKeyboard.java
@@ -4,7 +4,12 @@
  */
 package threadServer;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import ui.InputOutput;
 
 /**
@@ -12,6 +17,7 @@ import ui.InputOutput;
  * @author mariadefarges
  */
 public class ThreadToReadFromKeyboard implements Runnable {
+
     Socket socket;
 
     public ThreadToReadFromKeyboard(Socket socket) {
@@ -20,26 +26,50 @@ public class ThreadToReadFromKeyboard implements Runnable {
 
     @Override
     public void run() {
-        Boolean clientsConnected = true;
+        ServerThreads st = new ServerThreads();
         String option = null;
         while (true) {
+            
             System.out.println("1. Close the server.");
-            option = InputOutput.get_String();
+          option = InputOutput.get_String();
 
             if (option.equals("1")) {
                 System.out.println("Admin password: \n");
                 String password = InputOutput.get_String();
                 if (password.equals("12345")) {
-                    if (clientsConnected) {
-                        System.out.println("There are clients still connected. \n Are you sure you want to close the server? Y/N");
+                    if (st.sendClientsAlive() == 0) {
+                        System.out.println("There are no clients connected.\n The server has been closed successfully");
+                        st.releaseResourcesServer(st.getServerSocket());
+                        System.exit(0);
+                    } else {
+                        System.out.println("There are " + st.sendClientsAlive() + " clients still connected. \n Are you sure you want to close the server? Y/N");
                         option = InputOutput.get_String();
                         if (option.equals("Y")) {
+                            System.out.println("The server has been closed successfully");
+                            st.releaseResourcesServer(st.getServerSocket(), st.getSocket());
                             System.exit(0);
                         }
-
                     }
                 }
             }
+        }
+    }
+
+    public static void releaseResources(ServerSocket serverSocket, Socket socket, Socket socket2) {
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerThreads.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            socket2.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerThreads.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            serverSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerThreads.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
